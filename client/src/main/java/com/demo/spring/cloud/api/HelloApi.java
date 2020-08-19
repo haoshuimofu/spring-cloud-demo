924305ab328a30db10c9a728d8691d3bddd9e824
haoshuimofu <279759300@qq.com>
2020-08-19 16:49:09 +0800

    demo

--- client/src/main/java/com/demo/spring/cloud/api/HelloApi.java
+++ client/src/main/java/com/demo/spring/cloud/api/HelloApi.java
@@ -9,10 +9,10 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @author wude
  * @date 2020/4/29 10:57
  */
-@RequestMapping("/api")
+@RequestMapping("/api/hello")
 public interface HelloApi {
 
-    @GetMapping("/sayHello")
+    @GetMapping("/say")
     JsonResult<String> sayHello(@RequestParam(value = "name", defaultValue = "none") String name);
 
 }
